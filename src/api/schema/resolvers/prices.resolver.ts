4ff57c5717d9bc36a949659f1dcd33e92b6ea38f
Martin PELCAT <martinpelcat@hotmail.com>
2021-12-15 15:16:57 +0100

    delete dupicate

--- src/api/schema/resolvers/prices.resolver.ts
+++ src/api/schema/resolvers/prices.resolver.ts
@@ -12,28 +12,6 @@ builder.objectType(PriceObject, {
   }),
 });
 
-builder.mutationField('createPrice', (t) =>
-  t.field({
-    type: PriceObject,
-    authScopes: { isAdmin: true },
-    args: {
-      eventUuid: t.arg.string(),
-      amount: t.arg.float(),
-      description: t.arg.string({ required: false }),
-    },
-    resolve: async (_, args, { dataSources }) => {
-      const event = await prisma.event.findUnique({
-        where: { uuid: args.eventUuid },
-      });
-
-      return dataSources.price.createPrice(event.uuid, {
-        amount: args.amount,
-        description: args.description ?? undefined,
-      });
-    },
-  })
-);
-
 builder.mutationField('deletePrice', (t) =>
   t.field({
     type: PriceObject,
